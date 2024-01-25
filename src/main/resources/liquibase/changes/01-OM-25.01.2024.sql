8d804ef22f785a30d4560cc2e28764bdea9d6c7f
arcadag <piknik0711@yandex.ru>
2024-01-26 00:44:54 +0300

    add liquibase

--- src/main/resources/liquibase/changes/01-OM-25.01.2024.sql
+++ src/main/resources/liquibase/changes/01-OM-25.01.2024.sql
@@ -4,6 +4,7 @@ create table orders
   order_number      varchar(255),
   order_name        varchar(255),
   current_sum       decimal(10, 2),
+  order_state       varchar(750),
   master_status     varchar(75),
   completion_date   timestamp without time zone,
   created_date      timestamp without time zone not null,
